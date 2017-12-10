4baec1b0824018c77e4a95573b6c6d4ee6129a47
Fabio <fabio_dlc96@hotmail.com>
2017-12-10 17:46:29 -0400

    ultima clase

---
+++ app/src/main/java/edu/itla/tripdom/dao/PublicacionesDbo.java
@@ -0,0 +1,8 @@
+package edu.itla.tripdom.dao;
+
+/**
+ * Created by fabio on 12/10/2017.
+ */
+
+public class PublicacionesDbo {
+}
---
+++ app/src/main/java/edu/itla/tripdom/view/listadapter/PublicacionListAdapter.java
@@ -0,0 +1,8 @@
+package edu.itla.tripdom.view.listadapter;
+
+/**
+ * Created by fabio on 12/10/2017.
+ */
+
+public class PublicacionListAdapter {
+}
